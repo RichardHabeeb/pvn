2ac7b1bfd177aeb3123b86dad1eb4c0f3795551c
Richard <richard.habeeb@gmail.com>
2013-07-25 19:18:41 -0500

    mongo testing

--- index.php
+++ index.php
@@ -1,23 +1,25 @@
 <?php
-  # get the mongo db name out of the env
-  $mongo_url = parse_url(getenv("MONGOHQ_URL"));
-  $dbname = str_replace("/", "", $mongo_url["path"]);
+	# get the mongo db name out of the env
+	$mongo_url = parse_url(getenv("MONGOHQ_URL"));
+	$dbname = str_replace("/", "", $mongo_url["path"]);
+	echo $mongo_url. "<br>". $dbname;
+	/*
+	# connect
+	$m   = new Mongo(getenv("MONGOHQ_URL"));
+	$db  = $m->$dbname;
+	$col = $db->access;
 
-  # connect
-  $m   = new Mongo(getenv("MONGOHQ_URL"));
-  $db  = $m->$dbname;
-  $col = $db->access;
+	# insert a document
+	$visit = array( "ip" => $_SERVER["HTTP_X_FORWARDED_FOR"] );
+	$col->insert($visit);
 
-  # insert a document
-  $visit = array( "ip" => $_SERVER["HTTP_X_FORWARDED_FOR"] );
-  $col->insert($visit);
+	# print all existing documents
+	$data = $col->find();
+	foreach($data as $visit) {
+	echo "<li>" . $visit["ip"] . "</li>";
+	}
 
-  # print all existing documents
-  $data = $col->find();
-  foreach($data as $visit) {
-    echo "<li>" . $visit["ip"] . "</li>";
-  }
-
-  # disconnect
-  $m->close();
+	# disconnect
+	$m->close();
+	*/
 ?>
