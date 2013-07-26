26770b28d00994ccc58e31a1b15cc9aa7b46c311
Richard <richard.habeeb@gmail.com>
2013-07-26 14:21:48 -0500

    still messing with mongo. I got the local debugger helping out now.

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
-echo "1";
+
+$debug_logging = true;
+
+if($debug_logging) echo "PVN Start.<br>";
+
 require_once 'mongo.php';
 require_once 'entity.php';
 
 $player = new Entity();
-echo $player;
+print_r($player);
 
+if($debug_logging) echo "PVN End.";
 /* End of index.php */
--- mongo.php
+++ mongo.php
@@ -1,15 +1,22 @@
 <?php
 
-
-# get the mongo db name out of the env
 $mongo_url = parse_url(getenv("MONGOLAB_URI"));
+echo getenv("MONGOLAB_URI");
+
+if (!isset($mongo_url["path"]) || trim($mongo_url["path"]) == "") {
+	$mongo_url = parse_url("mongodb://test:test@ds037698.mongolab.com:37698/heroku_app17126981");
+}
+
 $dbname = str_replace("/", "", $mongo_url["path"]);
 
-
-# connect
-$m   = @new Mongo(getenv("MONGOLAB_URI")) or
-    die ("Failed opening file: error was '$php_errormsg'");
-$db  = $m->$dbname;
-$col = $db->access;
+try {
+	$m   = new Mongo(getenv("MONGOLAB_URI"));
+	$db = $m->$dbname;
+	$col = $db->access;
+} catch(exception $e) {
+	print_r($e);
+}
+	
+if($debug_logging) echo "Mongo loaded. ";
 
 /* End of mongo.php */
