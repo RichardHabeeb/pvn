7c1aa91446ee5058a88d4df75249f363673d947d
Richard <richard.habeeb@gmail.com>
2013-07-25 19:33:39 -0500

    mongodb testing

--- index.php
+++ index.php
@@ -1,23 +1,27 @@
 <?php
-	# get the mongo db name out of the env
-	$mongo_url = parse_url(getenv("MONGOHQ_URL"));
-	$dbname = str_replace("/", "", $mongo_url["path"]);
+	try {
+		# get the mongo db name out of the env
+		$mongo_url = parse_url(getenv("MONGOHQ_URL"));
+		$dbname = str_replace("/", "", $mongo_url["path"]);
 
-	# connect
-	$m   = new Mongo(getenv("MONGOHQ_URL"));
-	$db  = $m->$dbname;
-	$col = $db->access;
+		# connect
+		$m   = new Mongo(getenv("MONGOHQ_URL"));
+		$db  = $m->$dbname;
+		$col = $db->access;
 
-	# insert a document
-	$visit = array( "ip" => $_SERVER["HTTP_X_FORWARDED_FOR"] );
-	$col->insert($visit);
+		# insert a document
+		$visit = array( "ip" => $_SERVER["HTTP_X_FORWARDED_FOR"] );
+		$col->insert($visit);
 
-	# print all existing documents
-	$data = $col->find();
-	foreach($data as $visit) {
-	echo "<li>" . $visit["ip"] . "</li>";
+		# print all existing documents
+		$data = $col->find();
+		foreach($data as $visit) {
+		echo "<li>" . $visit["ip"] . "</li>";
+		}
+
+		# disconnect
+		$m->close();
+	} catch (Exception $e) {
+		echo "Exception: ", $e->getMessage(), "\n";
 	}
-
-	# disconnect
-	$m->close();
 ?>
