5ad1c83e3f49c4990ae50c58bdcdca3e5afe3bda
Richard <richard.habeeb@gmail.com>
2013-07-25 20:54:37 -0500

    MONGO IS WORKING!

--- index.php
+++ index.php
@@ -1,33 +1,15 @@
 <?php
-	try {
-		# get the mongo db name out of the env
-		$mongo_url = parse_url(getenv("MONGOLAB_URI"));
-		$dbname = str_replace("/", "", $mongo_url["path"]);
-		
-		print_r( $mongo_url["path"] . "<br />");
-		echo exec("pwd") . "<br />";
-		echo exec("ls -l") . "<br />";
-		
-		
-		# connect
-		$m   = new Mongo(getenv("MONGOLAB_URI"));
-		$db  = $m->$dbname;
-		$col = $db->access;
-		
-		# insert a document
-		$visit = array( "ip" => $_SERVER["HTTP_X_FORWARDED_FOR"] );
-		$col->insert($visit);
 
-		# print all existing documents
-		$data = $col->find();
-		foreach($data as $visit) {
-		echo "<li>" . $visit["ip"] . "</li>";
-		}
+# get the mongo db name out of the env
+$mongo_url = parse_url(getenv("MONGOLAB_URI"));
+$dbname = str_replace("/", "", $mongo_url["path"]);
 
-		# disconnect
-		$m->close();
-		/**/
-	} catch (Exception $e) {
-		echo "Exception: ", $e->getMessage(), "\n";
-	}
+
+# connect
+$m   = new Mongo(getenv("MONGOLAB_URI"));
+$db  = $m->$dbname;
+$col = $db->access;
+
+
+
 ?>
